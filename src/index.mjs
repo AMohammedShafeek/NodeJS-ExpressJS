00d1004ee4dec403d1baf35715a4a84e064703ed
AMohammedShafeek <amdshafeek7@gmail.com>
2025-12-20 11:33:54 +0530

    ExpressApp: Query Params

--- src/index.mjs
+++ src/index.mjs
@@ -4,28 +4,63 @@ const app = express()
 
 const PORT = 3000
 
+//Users
 const users = [
     {
         id: 1,
-        userName: 'Mohammed Shafeek. A'
+        user_name: 'Mohammed Shafeek. A'
     },
     {
         id: 2,
-        userName: 'Malavika. G'
+        user_name: 'Malavika. G'
     },
     {
         id: 3,
-        userName: 'Sivaponnuvel .S'
+        user_name: 'Sivaponnuvel .S'
     },
 ]
+
+//Products
+const products = [
+    {
+        id: 1,
+        product_name: 'Samsung Galexy A35'
+    },
+    {
+        id: 2,
+        product_name: 'Samsung Galexy S23 Ultra'
+    },
+    {
+        id: 3,
+        product_name: 'Samsung Galexy Flip'
+    },
+]
+
 app.get('/', (req, res) => {
     res.send({ msg: "Root" })
 })
 
+// Query Params
+//users
 app.get('/api/users', (req, res) => {
-    res.send(users)
+    const {query:{filter, value}} = req
+    if(filter && value){
+        return res.send(users.filter(((user)=>user[filter].toLowerCase().includes(value.toLowerCase()))))
+    }
+    res.send({msg: 'User Not Found'})
 })
 
+//products
+app.get('/api/products', (req, res) => {
+    const {query:{filter, value}} = req
+    if(filter && value){
+        return res.send(products.filter(((product)=>product[filter].toLowerCase().includes(value.toLowerCase()))))
+    }
+    res.send({msg: 'Product Not Found'})
+})
+
+// Route Params
+//users endpoint
 app.get('/api/users/:id', (req, res) => {
     const id = parseInt(req.params.id)
     if (isNaN(id)) {
@@ -40,7 +75,21 @@ app.get('/api/users/:id', (req, res) => {
     return res.status(404).send({ msg: "User Not Found" })
 })
 
+//product endpoint
+app.get('/api/products/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (isNaN(id)) {
+        return res.status(400).send({ msg: "Bad Request. Invalid ID" })
+    }
+    const product = products.find((product) =>
+        (product.id === id)
+    )
+    if (product) {
+        return res.send(product)
+    }
+    return res.status(404).send({ msg: "User Not Found" })
+})
+
 app.listen(PORT, () => {
     console.log(`App is Running on PORT: ${PORT}`);
-
 })
