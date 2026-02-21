a8c7c978b3ba5758736186a8364ec6ed4276a0ca
AMohammedShafeek <amdshafeek7@gmail.com>
2026-02-22 00:58:29 +0530

    CHAPTER-1: BASICS

---
+++ CHAPTERS/C1-BASICS/CLogs.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+
+console.log(global);
+
+console.log(__filename);
+console.log(__dirname);
---
+++ CHAPTERS/C1-BASICS/index.js
@@ -0,0 +1,9 @@
+const math = require('./Math')
+const { mod } = require('./Math')
+
+console.log(math.add(2,4));
+console.log(math.sub(2,4));
+console.log(math.mul(2,4));
+console.log(math.div(2,4));
+
+console.log(mod(2,4));
